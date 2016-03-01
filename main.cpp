149608897908b33c3a1e9f496d07b488774cf9a3
Justin <oszcoszc@gmail.com>
2016-03-01 18:41:53 +0800

    gfdgdfsdfsdfsdf

--- main.cpp
+++ main.cpp
@@ -41,6 +41,7 @@ int simplest_yuv420_split(char* url, int w, int h, int num){
 }
 
 int main() {
+    simplest_yuv420_split("lena_256x256_yuv420p.yuv",256,256,1);
     cout << "Hello, World!" << endl;
     return 0;
 }
